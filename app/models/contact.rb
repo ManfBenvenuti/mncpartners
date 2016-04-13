43b5644e7806ad31767128a922ddc05325539ed5
Manfredi <manfbenvenuti@gmail.com>
2016-04-13 17:53:10 +0200

    Sendgrid Fix Variables

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,13 +5,15 @@ class Contact < MailForm::Base
   attribute :telephone
   attribute :message
 
+  append :telephone
+
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
   def headers
     {
       :subject => "Contact form",
       :to => "manfbenvenuti@gmail.com",
-      :from => %("#{name}" <#{email}>)
+      :from => %("#{first_name}" "#{last_name}" <#{email}>)
     }
   end
 end
